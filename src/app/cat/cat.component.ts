4fe42c72d14ba5f1df7a999e80f29252e272e252
Sooraj M <soorajmappidachery@gmail.com>
2021-09-24 18:27:43 +0530

    optimized methods

--- src/app/cat/cat.component.ts
+++ src/app/cat/cat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SeoserviceService } from '../seoservice.service'
 
 @Component({
   selector: 'app-cat',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private seo:SeoserviceService) { }
 
   ngOnInit(): void {
+    this.seo.setPrimaryMetaTags("Dogs and Cats","You love dogs and cats, Don't you?")
+    this.seo.setFacebookMetaTags(window.location.href,"Dogs and Cats","You love dogs and cats, Don't you?","https://dogsculture.com/wp-content/uploads/2019/02/labrador-retriever.jpg")
+    this.seo.setTwitterMetaTags(window.location.href,"Dogs and Cats","You love dogs and cats, Don't you?","https://dogsculture.com/wp-content/uploads/2019/02/labrador-retriever.jpg")
+    var head=document.getElementsByTagName('head')[0]
+    var element=document.querySelector("link[rel='canonical'") || null
+    if(element==null){
+      var ele=document.createElement("link")
+      head.appendChild(ele)
+      ele.setAttribute('rel','canonical')
+      ele.setAttribute('href',window.location.href)
+    }
+  }
+  facebookSharer(){
+    console.log(window.location.href)
+    window.open("http://www.facebook.com/sharer/sharer.php?u="+window.location.href,"_blank")
+  }
+  twitterSharer(){
+    console.log(window.location.href)
+    window.open("https://twitter.com/intent/tweet?url="+window.location.href,"_blank")
   }
 
 }
