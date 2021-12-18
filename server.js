6c3e8feea65384ab893c165cd7236746f3510f52
Sooraj M <soorajmappidachery@gmail.com>
2021-12-18 22:39:59 +0530

    log user agent

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/angular-heroku'));
 app.get('/*', function(req,res) {
+  console.log(req.headers['user-agent']);
   res.sendFile(path.join(__dirname+
     '/dist/angular-heroku/index.html'));});
 app.listen(process.env.PORT || 8080);
